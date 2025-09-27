b43c211b1c34de8710a50aa7aa75e46124a7d17a
lgustavosam <lgustavosam@gmail.com>
2025-09-27 01:30:12 -0300

    Finalização do projeto

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MauiAppTempoAgora.Models;
 using MauiAppTempoAgora.Services;
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace MauiAppTempoAgora
@@ -8,12 +10,9 @@ namespace MauiAppTempoAgora
     {
         int count = 0;
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }        
+        public MainPage() => InitializeComponent();
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
             try
             {
@@ -34,6 +33,16 @@ namespace MauiAppTempoAgora
 
 
                         lbl_res.Text = dados_previsão;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=mm&metricTemp=°C" +
+                                      $"&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        mv_mapa.Source = mapa;
+                        Debug.WriteLine(mapa);
+
+
                     } else
                     {
                         lbl_res.Text = "Sem dados de Previsão";
@@ -49,6 +58,89 @@ namespace MauiAppTempoAgora
                 await DisplayAlert("Ops", ex.Message, "OK");
             }
         }
+
+
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest(
+                        GeolocationAccuracy.Medium,
+                        TimeSpan.FromSeconds(10)
+                        );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if (local != null)
+                {
+                    lbl_coords.Text = $"Latitude: {local.Latitude} \n" +
+                                        $"Longitude: {local.Longitude}";
+
+                    lbl_coords.Text = local_disp;
+
+                    //pega nome da cidade que está nas coordenadas
+                    await GetCidade(local.Latitude, local.Longitude);
+
+                  
+
+
+                    lbl_coords.Text += $"\nCidade: {txt_cidade.Text}";
+
+                } else
+                {
+                    lbl_coords.Text = "Nenhuma localização";
+                }
+
+            } catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não Suporta", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneex)
+            {
+                await DisplayAlert("Erro: Localização Desabilitada", fneex.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão da Localização", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+
+        }
+        private async Task GetCidade(double lat, double lon)
+        {
+            try
+            {
+
+
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+
+                
+                Placemark? place = places.FirstOrDefault();                
+
+                if (place != null)
+                {
+                    string cidade = place.Locality ?? place.SubAdminArea ?? place.AdminArea ?? place.CountryName ?? "Cidade não encontrada";
+                    txt_cidade.Text = cidade;
+                    Debug.WriteLine($"Cidade resolvida: {cidade}");
+                }
+                else
+                {
+                    txt_cidade.Text = "Cidade não encontrada";
+                    Debug.WriteLine("Nenhum placemark encontrado.");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: Obtenção do nome da Cidade", ex.Message, "OK");
+            }
+        }
     }
+    
 
 }
+
+
